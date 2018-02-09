4c02f33f73119c7782c65090c9a27159bc82da1c
최병민(01045842518) <byung803@gmail.com>
2018-02-09 18:35:44 +0900

    Change text on index.html and LoginPage

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Boilerplate</h1>
-            <p>Tag line for app.</p>
+            <h1 className="box-layout__title">Condition Regulator</h1>
+            <p>Track your condition and Change your life.</p>
             <button
                 className="button"
                 onClick={startLogin}
